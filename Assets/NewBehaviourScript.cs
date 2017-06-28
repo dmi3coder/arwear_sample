707cda0deb8391d68526af93cf25ea3c9357f7e6
dmi3coder <dmi3coder@gmail.com>
2017-06-28 16:55:32 +0300

    hello AR text 🤠

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour {
+	int i = 0;
+
+	
+	void Start () {
+		InvokeRepeating("timer_Tick", 0, 1);
+
+	}
+
+	void timer_Tick()
+	{
+		TextMesh text = GetComponent<TextMesh>();
+		text.text = DateTime.Now.ToString ("h:mm:ss tt");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
